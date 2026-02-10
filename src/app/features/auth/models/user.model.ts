00a5cabb81d0f81426103313b3cba5c8ae21a99d
ismail baoud <ibaoud46@egmail.com>
2026-02-10 09:45:36 +0100

    feat: add User and Login models for authentication

---
+++ src/app/features/auth/models/user.model.ts
@@ -0,0 +1,13 @@
+/* user model */
+
+export interface User{
+  id:Number
+  firstName:string,
+  lastName:string,
+  email:string,
+  password:string
+}
+
+export interface Login{
+  email:string,
+}
