cd1b32a95719faabfa68d730324547f1a0b05b80
ismail baoud <ibaoud46@egmail.com>
2026-02-10 09:46:03 +0100

    feat: configure application routes with auth guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/auth/components/register/register.component';
+import { LoginComponent } from './features/auth/components/login/login.component';
+import { LandingPageComponent } from './features/landingPage/components/landing-page/landing-page.component';
+import { guestGuard } from './core/guards/guest.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '' , component: LandingPageComponent},
+    { path: 'auth' , canActivateChild: [guestGuard],
+        children : [
+            {path: 'login', component: LoginComponent},
+            {path: 'register', component: RegisterComponent}
+        ]
+    },
+];
